dd615e0f3a700ffd85a2ce6659af0d55963707bc
AlexFullstackCourse <alexander.heidorn@padis.io>
2023-04-17 16:20:21 +0200

    Created a command line application that stores contacts to a MongoDB database

---
+++ mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const numberOfCmdLineArgs = process.argv.length;
+
+if (numberOfCmdLineArgs < 3) {
+  console.log(
+    "give password, name (in quotes) and number OR just the passwort as argument"
+  );
+  process.exit(1);
+} else {
+  const password = process.argv[2];
+
+  const url = `mongodb+srv://alexanderheidorn:${password}@cluster0.h6hnwap.mongodb.net/?retryWrites=true&w=majority`;
+
+  mongoose.set("strictQuery", false);
+  mongoose.connect(url);
+
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  });
+
+  const Person = mongoose.model("Person", personSchema);
+
+  if (numberOfCmdLineArgs > 3) {
+    const newName = process.argv[3];
+    const newNumber = process.argv[4];
+
+    const person = new Person({
+      name: newName,
+      number: newNumber,
+    });
+
+    person.save().then((result) => {
+      console.log(
+        `Added ${newName} with phone number ${newNumber} to phonebook!`
+      );
+      mongoose.connection.close();
+    });
+  } else {
+    console.log("Phonebook:");
+    Person.find({}).then((result) => {
+      result.forEach((person) => {
+        console.log(person.name, person.number);
+      });
+      mongoose.connection.close();
+    });
+  }
+}
